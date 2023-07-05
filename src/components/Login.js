0bedb9a3c29ad01323a7ca5c797bd8030c5bcd8a
jiyujin <yujin1679@naver.com>
2023-07-05 16:02:24 +0900

    add: Login 컴포넌트 마크업 추가

---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Login = props => {
+  return (
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="card w-96 m-auto px-3 shadow-sm bg-white">
+        <div className="card-body">
+          <h2 className="card-title mx-auto text-2xl font-extrabold mb-4">로그인</h2>
+          <input
+            type="text"
+            placeholder="이름을 입력해 주세요"
+            className="input input-bordered w-full max-w-xs text-sm"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+            className="input input-bordered w-full max-w-xs text-sm"
+          />
+          <div className="card-actions w-full">
+            <button className="btn bg-slate-700 hover:bg-slate-600 text-white w-full">로그인</button>
+          </div>
+          <p className="m-auto text-sm py-2">다른 방법으로 로그인</p>
+          <button className="btn bg-white hover:bg-gray-50 input-bordered text-center font-normal relative">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/150px-Google_%22G%22_Logo.svg.png"
+              className="absolute top-1/2 left-3 transform -translate-y-1/2 w-7 h-7.5"
+            />
+            구글로 로그인
+          </button>
+          <a className="link text-sm text-center py-2">회원가입</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
