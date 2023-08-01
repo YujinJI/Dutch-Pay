7e0895de625dfd30a3f0775fac2b2402993c5228
jiyujin <yujin1679@naver.com>
2023-08-01 22:07:12 +0900

    add: 특정 방의 사용자 목록을 렌더링하는 기능 추가

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,27 +1,32 @@
+import AvatarImage from '../assets/images/avatar.png';
 import React from 'react';
-import AvatarImage from '../assets/images/avatar.jpeg';
 
-const MemberList = props => {
+const MemberList = ({ usersName, usersAccount, user }) => {
+  // 매개변수 구조 분해로 변경
+  const userNameArray = Object.values(usersName); // 객체를 배열로 변환
+
   return (
     <div className="w-56 text-center bg-[#40798C]">
       <div>
         <h2 className="text-xl font-extrabold text-white my-4">참여자 목록</h2>
-        <div className="flex justify-center items-center my-2">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar mx-2">
-            <div className="w-10 rounded-full">
-              <img src={AvatarImage} alt="avatar image" />
+        {userNameArray.map((userName, index) => (
+          <div key={index} className="relative">
+            <div className="flex flex-wrap justify-center items-center mt-2">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar mx-2">
+                <div className="w-10 rounded-full">
+                  {user.photoURL ? <img src={user.photoURL} /> : <img src={AvatarImage} alt="avatar image" />}
+                  {/*<img src={AvatarImage} alt="avatar image" />*/}
+                </div>
+              </label>
+              <p className="mx-2 text-white">{userName}</p>
             </div>
-          </label>
-          <p className="mx-2 text-white">지유진</p>
-        </div>
-        <div className="flex justify-center items-center my-2">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar mx-2">
-            <div className="w-10 rounded-full">
-              <img src={AvatarImage} alt="avatar image" />
-            </div>
-          </label>
-          <p className="mx-2 text-white">지유진</p>
-        </div>
+            {usersAccount && (
+              <p className="text-white text-sm pb-3">
+                {usersAccount[index].bank}/{usersAccount[index].account}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
