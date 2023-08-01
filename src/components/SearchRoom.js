d93e59ac7ce3c5e4c491dfda5a89eb01c66f8637
jiyujin <yujin1679@naver.com>
2023-08-01 21:59:32 +0900

    add: 방 코드 입력 후 일치 여부 확인하는 기능 추가

--- src/components/SearchRoom.js
+++ src/components/SearchRoom.js
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { addUserToRoom, findRoomByCode, updateRoomCode } from '../service/database';
+import AuthContext from '../context/AuthContext';
 
 const SearchRoom = props => {
+  const { user } = useContext(AuthContext);
   const [roomCode, setRoomCode] = useState('');
+  const [roomName, setRoomName] = useState('');
 
-  const generateRandomString = () => {
-    return 'random_string';
+  const handleInputRoomName = e => {
+    setRoomName(e.target.value);
   };
 
-  const handleClickCreateRoom = () => {
-    const randomString = generateRandomString();
-    window.location.href = `/rooms/${randomString}`;
+  const handleClickCreateRoom = async () => {
+    const roomCode = await updateRoomCode(user, roomName);
+    window.location.href = `/rooms/${roomCode}`;
   };
 
   const handleCodeChange = e => {
     setRoomCode(e.target.value);
   };
 
-  const handleSubmitCode = () => {};
+  const handleSubmitCode = async () => {
+    if (roomCode.trim() === '') {
+      // 방 코드가 입력되지 않았을 경우, 처리 로직 추가
+      alert('방 코드를 입력해주세요.');
+      return;
+    }
+
+    const roomData = await findRoomByCode(roomCode);
+
+    if (roomData) {
+      // 사용자가 입력한 방 코드와 일치하는 방을 찾았을 경우
+      const roomName = await addUserToRoom(roomCode, user);
+      // 이후 원하는 동작 수행 (예: 해당 방으로 이동하는 등)
+      window.location.href = `/rooms/${roomCode}`;
+    } else {
+      // 사용자가 입력한 방 코드와 일치하는 방이 없을 경우, 처리 로직 추가
+      alert('입력한 방 코드와 일치하는 방이 없습니다.');
+    }
+  };
 
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -51,10 +72,11 @@ const SearchRoom = props => {
                   type="text"
                   placeholder="모임 이름 입력"
                   className="input input-bordered w-full max-w-xs my-2 text-base"
+                  onChange={handleInputRoomName}
                 />
                 <div className="modal-action">
                   <button className="btn bg-slate-700 hover:bg-slate-600 text-white" onClick={handleClickCreateRoom}>
-                    <Link to={`rooms/${generateRandomString()}`}>완료</Link>
+                    완료
                   </button>
                 </div>
               </form>
